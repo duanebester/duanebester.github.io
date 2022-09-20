f1cc43b10dc5f80347c784ed5ad19a18fe3b7022
Duane Bester <bester.duane@gmail.com>
2022-09-20 17:54:24 -0500

    styles and dark mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+import react from "@astrojs/react";
+import image from "@astrojs/image";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://duanebester.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://duanebester.com',
+  integrations: [mdx(), sitemap(), react(), image(), tailwind()]
 });
